fbf840295f0e0372930fdf011a62e78be1e376a1
= <=>
2016-08-09 11:54:51 +0200

    inicio y about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+  #definimos la nueva pagina
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #get 'pages/home'
   root 'pages#home'
+  #COn esto indicamos la ruta de la pagina about
+  get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
